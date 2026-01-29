952793dfca92837eb8becc6982b88593321de4e0
MELQUIADES-art <melquiades.rodriguez@uva.es>
2026-01-29 13:05:34 +0100

    Update entrenamiento.py

--- entrenamiento.py
+++ entrenamiento.py
@@ -4,8 +4,6 @@ from sklearn.datasets import load_iris
 from sklearn.linear_model import Perceptron
 import pandas as pd
 iris = load_iris(as_frame=True)
-dt=pd.DataFrame(iris)
-dt.head(5)
 X = iris.data[["petal length (cm)", "petal width (cm)"]].values
 y = (iris.target == 0) # Iris setosa
 per_clf = Perceptron(random_state=42)
@@ -13,4 +11,3 @@ per_clf.fit(X, y)
 X_new = [[2, 0.5], [3, 1]]
 y_pred = per_clf.predict(X_new)
 y_pred
-dt[dt["petal length (cm)"]==2]
